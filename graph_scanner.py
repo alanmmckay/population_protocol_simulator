24b1f5f859036a82f3f7bd2f5c0e70d9fe3087ec
alanmmckay <alanmmckay@gmail.com>
2020-09-21 16:05:26 -0500

    Added logic in produceToken() to recognize each individual delimiter... Not sure I like the lengthy set of conditions. git add graph_scanner.py  Still need to remove test logic contained within this file git add graph_scanner.py

--- graph_scanner.py
+++ graph_scanner.py
@@ -10,6 +10,11 @@ print(graph_data)
 #-objects which are referenced internally use underscores
 
 delimiters = ["{","}","(",")",","]
+valid_vertex = list()
+for i in range(97,123):
+    valid_vertex.append(chr(i))
+for i in range(0,10):
+    valid_vertex.append(i)
 
 class GraphScanner:
     def __init__(self, graph_str):
@@ -23,29 +28,71 @@ class GraphScanner:
         
     def peek(self):
         if not self.look_ahead:
-            self.lookahead = self.getNextToken()
+            self.look_ahead = self.nextToken()
         return self.look_ahead
     
-   def nextToken(self):
-       if self.look_ahead:
-           next_token = self.look_ahead
-           self.look_ahead = None
-           return next_token
-       else:
-           return self.getNextToken()
+    def nextToken(self):
+        if self.look_ahead:
+            next_token = self.look_ahead
+            self.look_ahead = None
+            return next_token
+        else:
+            return self.getNextToken()
        
+    def getVertex(self):
+        #while the next token is an alpha numeric character
+        vertex_string = str()
+        offset = self.pos
+        while True:
+            if self.graph_str[offset] in valid_vertex:
+                vertex_string += self.graph_str[offset]
+                offset += 1
+            else:
+                return vertex_string
+    
+    def produceToken(self):
+        if self.pos >= self.maxPos:
+            return GraphToken(GraphTokenType.EOF)
+        elif self.graph_str[self.pos] in delimiters:
+            #Adhocery: {
+            if self.graph_str[self.pos] == '(':
+                return GraphToken(GraphTokenType.OPENPAREN, \
+                   self.graph_str[self.pos])
+            elif self.graph_str[self.pos] == ')':
+                return GraphToken(GraphTokenType.CLOSEPAREN, \
+                   self.graph_str[self.pos])
+            elif self.graph_str[self.pos] == ',':
+                return GraphToken(GraphTokenType.COMMA, \
+                   self.graph_str[self.pos])
+            elif self.graph_str[self.pos] == '{':
+                return GraphToken(GraphTokenType.OPENCURLY, \
+                   self.graph_str[self.pos])
+            elif self.graph_str[self.pos] == '}':
+                return GraphToken(GraphTokenType.CLOSECURLY, \
+                   self.graph_str[self.pos])
+            #} can probably wrap this up into a dictionary.
+        else:
+            vertex = self.getVertex()
+            if vertex != '':
+                return GraphToken(GraphTokenType.VERTEX, \
+                       vertex)
+            else:
+                raise ValueError("Invalid vertex label")
+            
     def getNextToken(self):
         #possibly add a method which skips irrelevant whitespace and comments
-        if self.pos >= maxPos:
-            return GraphToken(GraphTokenType.EOF)
-        
-        elif self.graph_str[self.pos] in delimiters:
-            return GraphToken(GraphTokenType.DELIMITER,self.graph_str[self.pos])
-        
-        elif 
-        #check to see valid vertex label.
-        #check to see if it is a duplicate.
-        
+        token = self.produceToken()
+        if token.isVertex():
+            self.pos += len(token.getValue())
         else:
-            #throw an error
-    
+            self.pos += 1
+        return token
+            
+graph = GraphScanner(graph_data)
+
+while True:
+    token = graph.getNextToken()
+    print(token)
+    if token.isEOF():
+        break
+
