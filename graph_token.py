408d13d56ae6cc325846eab946e993fe71f42b35
alanmmckay <alanmmckay@gmail.com>
2020-09-21 16:04:02 -0500

    Added a more granular set of definitions for the delimiters.

--- graph_token.py
+++ graph_token.py
@@ -2,8 +2,12 @@ from enum import Enum
 
 class GraphTokenType(Enum):
     VERTEX = 1;
-    DELIMITER = 2;
-    EOF = 3;
+    OPENPAREN = 2;
+    CLOSEPAREN = 3;
+    OPENCURLY = 4;
+    CLOSECURLY = 5;
+    COMMA = 6;
+    EOF = 7;
     
 class GraphToken:
     def __init__(self, token_type, token_value = None):
@@ -16,15 +20,36 @@ class GraphToken:
     def isDelimiter(self):
         return self.token_type == GraphTokenType.DELIMITER
     
+    def isOpenParen(self):
+        return self.token_type == GraphTokenType.OPENPAREN
+    
+    def isCloseParen(self):
+        return self.token_type == GraphTokenType.CLOSEPAREN
+    
+    def isOpenCurly(self):
+        return self.token_type == GraphTokenType.OPENCURLY
+    
+    def isCloseCurly(self):
+        return self.token_type == GraphTokenType.CLOSECURLY
+    
+    def isComma(self):
+        return self.token_type == GraphTokenType.COMMA
+    
     def isEOF(self):
         return self.token_type == GraphTokenType.EOF
+    
+    def getValue(self):
+        return self.token_value
+    
+    def getType(self):
+        return self.token_type
     
     def __repr__(self):
         if self.isVertex():
             return 'Vertex ' + str(self.token_value)
         
-        elif self.isDelimiter():
-            return 'Delimiter ' + str(self.token_value)
-        
-        else:
+        elif self.isEOF():
             return 'end_of_stream'
+
+        else:
+            return 'Delimiter ' + str(self.token_value)
