5b6b3f69100b972b155e543881dcbb2734b36ffb
alanmmckay <alanmmckay@gmail.com>
2020-09-22 16:26:09 -0500

    Hard reset

---
+++ graph_parser_test.py
@@ -0,0 +1,10 @@
+from graph_scanner import GraphScanner
+from graph_token import GraphToken, GraphTokenType
+from graph_parser import GraphParser
+from helper_functions import file_input
+
+graph_data = file_input('graph')
+print("Looking at graph: " + str(graph_data))
+scanner = GraphScanner(graph_data)
+parser = GraphParser(scanner)
+print(parser.parse_graph()) 
