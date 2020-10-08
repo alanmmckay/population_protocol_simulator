3d19910f84ba330e442e2834c4d2e29c2a2b0d9e
alanmmckay <alanmmckay@gmail.com>
2020-10-07 19:28:29 -0500

    The parser superclass now contains match, and match_encapsulator methods which are used to evaluate tokens. GraphParser now contains logic which is pertinent to the structure of a graph datatype input.

--- graph_parser.py
+++ graph_parser.py
@@ -1,13 +1,17 @@
+from parser import Parser
 from general_token import GeneralTokenType
 from errors import GeneralError
 
-class GraphParser:
+class GraphParser(Parser):
     def __init__(self,scanner):
-       self.scanner = scanner
-       self.vertex_set = list()
+        Parser.__init__(self, scanner)
+        self.vertex_set = list()
+        self.delimiter_set = scanner.getDelimiters()
+       
        
     def parse(self):
         return self.parse_graph()
+    
     
     def parse_graph(self):
         self.match(GeneralTokenType.DELIMITER, '(')
@@ -17,13 +21,13 @@ class GraphParser:
         self.match(GeneralTokenType.DELIMITER, ')')
         return (self.vertex_set, edge_set)
     
+    
     def parse_vertex_set(self):
         vertex_set = []
         self.match(GeneralTokenType.DELIMITER, '{')
         next_token = self.scanner.peek()
         #prime the loop
         if next_token.getValue() != '}':
-            #next_token = self.match_vertex()
             next_token = self.match(GeneralTokenType.STRING)
             vertex_set.append(next_token.getValue())
             next_token = self.scanner.peek()
@@ -31,7 +35,6 @@ class GraphParser:
                 #execute the loop
                 while next_token.getValue() == ',':
                     self.match(GeneralTokenType.DELIMITER, ',')
-                    #next_token = self.match_vertex()
                     next_token = self.match(GeneralTokenType.STRING)
                     if next_token.getValue() not in vertex_set:
                         vertex_set.append(next_token.getValue())
@@ -41,46 +44,38 @@ class GraphParser:
                         error_msg += next_token.getValue()
                         error_msg += "' found in vertex set."
                         GeneralError(error_msg, self.scanner.getInputString())
-                        #raise ValueError("error in parse_vertex_set")
             self.match(GeneralTokenType.DELIMITER, '}')
         return vertex_set
     
+    
     def parse_edge(self):
-        opener = self.match_open_pair()
-        #a = self.match_vertex().getValue()
+        #this usage of match_encapsulator could backfire if this type of data is
+        #expecting a value from a subset of the encapsulator superset
+        opener = self.match_encapsulator()
         a = self.match(GeneralTokenType.STRING).getValue()
         self.match(GeneralTokenType.DELIMITER, ',')
-        #b = self.match_vertex().getValue()
         b = self.match(GeneralTokenType.STRING).getValue()
-        closer = self.match_close_pair()
+        closer = self.match_encapsulator()
         
         if (a not in self.vertex_set) or (b not in self.vertex_set):
             error_msg = "Error in edge. Vertex contained in edge '"
             error_msg += str((a,b))
             error_msg += "' has not been declared within vertex set."
             GeneralError(error_msg, self.scanner.getInputString())
-            #raise ValueError("edge vertex not in vertex set")
         
-        if opener.getValue() == '(':
-            if closer.getValue() == ')':
-                return [(a,b)]
-            else:
-                error_msg = "Error in edge set. Expecting a closing "
-                error_msg += "parenthesis ')', received: '"
-                error_msg += closer.getValue() + "'"
-                GeneralError(error_msg, self.scanner.getInputString())
-                #raise ValueError('Paren Error in parse edge set')
-        elif opener.getValue() == '{':
-            if closer.getValue() == '}':
-                return [(a,b),(b,a)]
-            else:
-                error_msg = "Error in edge set. Expecting a closing "
-                error_msg += "curly bracket '}', received: '"
-                error_msg += closer.getValue() + "'"
-                GeneralError(error_msg, self.scanner.getInputString())
-                #raise ValueError('Curly Error in parse edge set')
-                
-                
+        if self.delimiter_set.validateEncapsulator(opener.getValue(), \
+            closer.getValue()):
+            pair = [(a,b)]
+            if opener.getValue() == '{':
+                pair.append((b,a))
+            return pair
+        else:
+            error_msg = "Error in edge set. Expecting a matching pair of encapsulating"
+            error_msg += " delimiters. Received: '"+str(opener.getValue())+"' and '"
+            error_msg += str(closer.getValue())+"'."
+            GeneralError(error_msg, self.scanner.getInputString())
+            
+            
     #let an edge be described as a tuple
     #an unordered pair will be considered a pair of two tuples
     def parse_edge_set(self):
@@ -118,54 +113,4 @@ class GraphParser:
                 next_token = self.scanner.peek()
         self.match(GeneralTokenType.DELIMITER, '}')
         return edge_set
-        
-    def match(self, expected_token_type, expected_token_value = None):
-        next_token = self.scanner.nextToken()
-        if next_token.getType() == expected_token_type:
-            if expected_token_value:
-                if next_token.getValue() == expected_token_value:
-                    return next_token
-                else:
-                    error_msg = "Unexpected token: '"
-                    error_msg += str(next_token)
-                    error_msg += "' found."
-                    GeneralError(error_msg, self.scanner.getInputString())
-                    #raise ValueError("error in match")
-            else:
-                return next_token
-        else:
-            error_msg = "Unexpected token type found in parser match"
-            GeneralError(error_msg, self.scanner.getInputString())
-            #raise ValueError("error in match")
-
-
     
-    def match_open_pair(self):
-        next_token = self.scanner.nextToken()
-        #can probably break out the .isDelimeter() methods here...
-        if (next_token.getValue() == '(') or \
-           (next_token.getValue() == '{'):
-            return next_token
-        else:
-            error_msg = "Error in within edge set. Expecting an opening "
-            error_msg += "delimiter. Received '"
-            error_msg += next_token.getValue()
-            error_msg += "' instead"
-            GeneralError(error_msg, self.scanner.getInputString())
-            #raise ValueError("error in match_open_pair")
-        
-    def match_close_pair(self):
-        next_token = self.scanner.nextToken()
-        #can probably break out the .isDelimeter() methods here...
-        if (next_token.getValue() == ')') or \
-           (next_token.getValue() == '}'):
-            return next_token
-        else:
-            error_msg = "Error in within edge set. Expecting an closing "
-            error_msg += "delimiter. Received '"
-            error_msg += next_token.getValue()
-            error_msg += "' instead"
-            GeneralError(error_msg, self.scanner.getInputString())
-            #raise ValueError("error in match_close_pair")
-
-        
---
+++ parser.py
@@ -0,0 +1,40 @@
+from general_token import GeneralTokenType
+from errors import GeneralError
+
+class Parser:
+    def __init__(self, scanner):
+        #expects a scanner with the Delimiters object type
+        self.scanner = scanner
+        
+    def parse(self):
+        pass
+        
+    def match(self, expected_token_type, expected_token_value = None):
+        next_token = self.scanner.nextToken()
+        if next_token.getType() == expected_token_type:
+            if expected_token_value:
+                if next_token.getValue() == expected_token_value:
+                    return next_token
+                else:
+                    error_msg = "Unexpected token: '"
+                    error_msg += str(next_token)
+                    error_msg +="' found."
+                    GeneralError(error_msg, self.scanner.getInputString())
+            else:
+                return next_token
+        else:
+            error_msg = "Unexpected token type found in parser match"
+            GeneralError(error_msg, self.scanner.getInputString())
+            
+    def match_encapsulator(self):
+        next_token = self.scanner.nextToken()
+        if next_token.getValue() in self.delimiter_set.getOpeners() or \
+            next_token.getValue() in self.delimiter_set.getClosers():
+                return next_token
+        else:
+            error_msg = "Error within edge set. Expecting an ecapsulating delimiter. "
+            error_msg += "Received '"
+            error_msg += next_token.getValue()
+            error_msg += "' instead."
+            GeneralError(error_msg, self.scanner.getInputString())
+            
